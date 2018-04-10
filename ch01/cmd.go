a067fe98c14082b0cb12155382dab4b98bd57066
ningwei <1152093468@qq.com>
2018-04-10 17:12:19 +0800

    编写命令行分析功能

---
+++ ch01/cmd.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+//读取命令行场数据的结构体
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	class       string
+	args        []string
+}
+
+//解析命令行参数，返回对应的结构体
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+
+	return cmd
+}
+
+//打印提示信息的函数
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args]\n", os.Args[0])
+}
